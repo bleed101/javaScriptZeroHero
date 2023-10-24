5e34b801062d2f3826ba4879e864c2f9a2371ebd
bleed101 <entender8ahora@gmail.com>
2023-10-25 01:51:10 +0530

    Sec2|08: This commit consist of using the Js two differnt ways.

---
+++ 01-Fundamentals-Part-1/starter/alert.js
@@ -0,0 +1,4 @@
+let js = 'javaScript';
+if(js==='javaScript') alert('This is the alert rasied by Js when if condition is met!!');
+40+7+8-10; //this won't work this way like we can just execute in console of browser if we want this to work we have to log it to console explicitly.
+console.log(40+7+8-10); //check in console it would have printed the output.
