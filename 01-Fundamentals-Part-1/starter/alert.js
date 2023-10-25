3489af2f090ab06c5a73fb5e12fcaf9dbec03d1d
bleed101 <entender8ahora@gmail.com>
2023-10-26 00:52:26 +0530

    sec02|09: variable naming convention

--- 01-Fundamentals-Part-1/starter/alert.js
+++ 01-Fundamentals-Part-1/starter/alert.js
@@ -2,3 +2,7 @@ let js = 'javaScript';
 if(js==='javaScript') alert('This is the alert rasied by Js when if condition is met!!');
 40+7+8-10; //this won't work this way like we can just execute in console of browser if we want this to work we have to log it to console explicitly.
 console.log(40+7+8-10); //check in console it would have printed the output.
+
+// 'new' and 'name' are reserved variables
+// Always start with smalecase letter in javascript var, for real constant you can use capilat letter 
+let PI=3.124
