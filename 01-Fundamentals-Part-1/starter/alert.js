d9cbb5d6acadcc92bf2fa2591522a7b861874230
bleed101 <entender8ahora@gmail.com>
2023-10-26 01:34:40 +0530

    sec02|14: Math operators, data types, precedence

--- 01-Fundamentals-Part-1/starter/alert.js
+++ 01-Fundamentals-Part-1/starter/alert.js
@@ -6,3 +6,55 @@ console.log(40+7+8-10); //check in console it would have printed the output.
 // 'new' and 'name' are reserved variables
 // Always start with smalecase letter in javascript var, for real constant you can use capilat letter 
 let PI=3.124
+
+//every value is object/primitive when it's not an object then it is primitive data type.
+//7 data types
+let age=23; //Number
+let name1='tipu'; //String
+let dead = true; //Boolean
+let moreHistory; //Undefined
+//Null empty value.
+//Symbol(ES2015) unique cannot be changed.
+//BigInt(ES2020) Larger integer than the Number type can hold.
+//Js is dynamic typing we do not have to specificaly difine the type.
+
+console.log(typeof age);
+console.log(typeof name1);
+console.log(typeof dead);
+console.log(typeof moreHistory);
+moreHistory = 'Exists'
+console.log(moreHistory);//it changed from undefined to string Dynamic typing
+
+const birthYear=1990;//itcan never be changed.
+var varString = 'function scoped and let is block scoped'; //it was used before ES6
+
+propertVar = 'this is not created under current scope'; //this has been created propety on global scope.
+
+//Math operators
+const now = 2023;
+const ageRam = now - 1995;
+const ageKara = now - 2000;
+console.log(ageRam, ageKara);
+
+console.log(ageRam*2, ageRam/10, ageRam**2); // performing some operation on age of Ram.
+
+const nameRam = 'Ram';
+const nameKara = 'Kara';
+
+console.log(nameRam + ' Weds ' + nameKara);
+
+let x = 10+5;
+x+=10;
+x*=4;
+x++;
+x--;
+console.log(x);
+
+console.log(ageRam>ageKara);
+console.log(ageKara >= 18);
+
+let y;
+x = y = 25 - 10 - 5;//right-to-left
+console.log(x,y); 
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
